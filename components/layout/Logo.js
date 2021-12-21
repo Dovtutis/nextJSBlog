c2a7bdf25a21b5dc1608a5666e66aa606a67d843
Dovtutis <dovtutis@gmail.com>
2021-12-21 20:27:42 +0200

    Layout with navigation and logo added

---
+++ components/layout/Logo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import classes from "./Logo.module.css";
+
+function Logo() {
+  return <div className={classes.logo}>Dovtutis Next Blog</div>;
+}
+
+export default Logo;
