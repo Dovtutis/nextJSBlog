29589873620fd988af6842103f2b0505c07d2fc3
Dovtutis <dovtutis@gmail.com>
2021-12-22 16:14:37 +0200

    Single post page added with its components

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,39 @@
 import React from "react";
 
-function SinglePostPage() {
-  return <div></div>;
+import PostContent from "../../components/posts/PostDetail/PostContent";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
+
+function SinglePostPage({ post }) {
+  return <PostContent post={post} />;
+}
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+}
+
+export function getStaticPaths() {
+  const postFilenames = getPostsFiles();
+
+  const slugs = postFilenames.map((filename) => filename.replace(/\.md$/, ""));
+
+  return {
+    paths: slugs.map((slug) => ({
+      params: {
+        slug: slug,
+      },
+    })),
+    fallback: false,
+  };
 }
 
 export default SinglePostPage;
